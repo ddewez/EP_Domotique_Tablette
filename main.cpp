45d174f4924d39d9e730697080e3aea22e69a2b9
ddewez <diane.dewez@insa-rennes.fr>
2016-04-18 15:31:01 +0200

    Toutes les fonctions mais erreur abort() 3 fois sur 4

--- main.cpp
+++ main.cpp
@@ -11,55 +11,154 @@
 
 using namespace std;
 
+int getVirtualJoystickPositionX(QSerialPort *port){
+    int x = 15;
+
+    const char output[10] = "AT+WGVP\r\n";
+    port->write(output);
+    port->flush();
+    QByteArray input = port->readAll();
+    while (port->waitForReadyRead(2000)) {
+       input.append(port->readAll());
+    }
+    std::regex pattern { "(\\d+),\\d+" };
+    std::string target { input.toStdString() };
+    std::smatch match;
+    std::regex_search(target, match, pattern);
+    x = boost::lexical_cast<int>(match[1]);
+
+    return x;
+}
+
+int getVirtualJoystickPositionY(QSerialPort *port){
+    int y = 15;
+    const char output[10] = "AT+WGVP\r\n";
+    port->write(output);
+    port->flush();
+    QByteArray input = port->readAll();
+    while (port->waitForReadyRead(2000)) {
+       input.append(port->readAll());
+    }
+    std::regex pattern { "\\d+,(\\d+)" };
+    std::string target { input.toStdString() };
+    std::smatch match;
+    std::regex_search(target, match, pattern);
+    y = boost::lexical_cast<int>(match[1]);;
+
+
+    return y;
+}
+
+int getMaximumSpeed(QSerialPort *port){
+    int speed = 15;
+    const char output[10] = "AT+WGMS\r\n";
+    port->write(output);
+    port->flush();
+    QByteArray input = port->readAll();
+    while (port->waitForReadyRead(2000)) {
+       input.append(port->readAll());
+    }
+    std::regex pattern { ":(\\d+)" };
+    std::string target { input.toStdString() };
+    std::smatch match;
+    std::regex_search(target, match, pattern);
+    speed = boost::lexical_cast<int>(match[1]);
+
+    return speed;
+}
+
+int getSpeed(QSerialPort *port){
+    int speed = 15;
+    int s1 = 0;
+    int s2 = 0;
+    const char output[10] = "AT+WGSP\r\n";
+    port->write(output);
+    port->flush();
+    QByteArray input = port->readAll();
+    while (port->waitForReadyRead(2000)) {
+       input.append(port->readAll());
+    }
+    std::regex pattern { ":(\\d+),(\\d+)" };
+    std::string target { input.toStdString() };
+    std::smatch match;
+    std::regex_search(target, match, pattern);
+    s1 = boost::lexical_cast<int>(match[1]);
+    s2 = boost::lexical_cast<int>(match[2]);
+    speed = (s2+s1)/256;
+    return speed;
+}
+
+int getProfile(QSerialPort *port){
+    int profile = 9;
+    const char output[10] = "AT+WGMD\r\n";
+    port->write(output);
+    port->flush();
+    QByteArray input = port->readAll();
+    while (port->waitForReadyRead(2000)) {
+       input.append(port->readAll());
+    }
+    std::regex pattern { ":(\\d+)" };
+    std::string target { input.toStdString() };
+    std::smatch match;
+    std::regex_search(target, match, pattern);
+    profile = boost::lexical_cast<int>(match[1]);
+
+    return profile;
+}
+
+int getBatteryLvl(QSerialPort *port){
+    int battery = 10;
+    const char output[10] = "AT+WGMD\r\n";
+    port->write(output);
+    port->flush();
+    QByteArray input = port->readAll();
+    while (port->waitForReadyRead(2000)) {
+       input.append(port->readAll());
+    }
+    std::regex pattern { ":(\\d+)" };
+    std::string target { input.toStdString() };
+    std::smatch match;
+    std::regex_search(target, match, pattern);
+    battery = boost::lexical_cast<int>(match[1]);
+
+    return battery;
+}
+
 int getJoystickPositionX(QSerialPort *port){
     int x = 15;
-    port->open(QIODevice::ReadWrite);
-        if(port->isOpen() && port->isWritable()){
-            const char output[10] = "AT+WGVP\r\n";
-            port->write(output);
-            port->flush();
-            QByteArray input = port->readAll();
-            while (port->waitForReadyRead(2000)) {
-               input.append(port->readAll());
-            }
-            qDebug() << input;
-            std::regex pattern { "(\\d+),\\d+" };
-            std::string target { input.toStdString() };
-            std::smatch match;
-            std::regex_search(target, match, pattern);
-            x = boost::lexical_cast<int>(match[1]);;
 
-
-    } else {
-        cout << "Erreur" << endl;
+    const char output[10] = "AT+WGVP\r\n";
+    port->write(output);
+    port->flush();
+    QByteArray input = port->readAll();
+    while (port->waitForReadyRead(2000)) {
+       input.append(port->readAll());
     }
-    port->close();
+    std::regex pattern { "(\\d+),\\d+" };
+    std::string target { input.toStdString() };
+    std::smatch match;
+    std::regex_search(target, match, pattern);
+    x = boost::lexical_cast<int>(match[1]);
+
     return x;
 }
 
 int getJoystickPositionY(QSerialPort *port){
     int y = 15;
-    if(port->open(QIODevice::ReadWrite)){
-        if(port->isOpen() && port->isWritable()){
-            const char output[10] = "AT+WGVP\r\n";
-            port->write(output);
-            port->flush();
-            QByteArray input = port->readAll();
-            while (port->waitForReadyRead(2000)) {
-               input.append(port->readAll());
-            }
-            qDebug() << input;
-            std::regex pattern { "\\d+,(\\d+)" };
-            std::string target { input.toStdString() };
-            std::smatch match;
-            std::regex_search(target, match, pattern);
-            y = boost::lexical_cast<int>(match[1]);;
-        }
-
-    } else {
-        cout << "Erreur" << endl;
+    const char output[10] = "AT+WGVP\r\n";
+    port->write(output);
+    port->flush();
+    QByteArray input = port->readAll();
+    while (port->waitForReadyRead(2000)) {
+       input.append(port->readAll());
     }
-    port->close();
+    std::regex pattern { "\\d+,(\\d+)" };
+    std::string target { input.toStdString() };
+    std::smatch match;
+    std::regex_search(target, match, pattern);
+    y = boost::lexical_cast<int>(match[1]);;
+
+
     return y;
 }
 
@@ -67,10 +166,19 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     QSerialPort *port = new QSerialPort("COM9");
-    int x = getJoystickPositionX(port);
-    int y = getJoystickPositionY(port);
-    cout << "Position du joystick = (" << x << ",";
-    cout << y << ")" << endl;
+    port->open(QIODevice::ReadWrite);
+    if(port->isOpen() && port->isWritable()){
+        int x = getJoystickPositionX(port);
+        int y = getJoystickPositionY(port);
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+        cout << "Vitesse : " << getSpeed(port) << endl;
+        cout << "Vitesse maximale : " << getMaximumSpeed(port) << endl;
+        cout << "Profil : " << getProfile(port) << endl;
+        cout << "Batterie : " << getBatteryLvl(port) << endl;
+    }
+    port->close();
     delete port;
+    a.quit();
 
 }
