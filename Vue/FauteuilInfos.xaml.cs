c65be20e302bc461334ce03b035ce0572dc730bd
ddewez <diane.dewez@insa-rennes.fr>
2016-04-29 12:53:24 +0200

    Commit 29/04

---
+++ Vue/FauteuilInfos.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Windows;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+namespace MyDomotik
+{
+
+    public sealed partial class FauteuilInfos : Page
+    {
+        
+              
+
+       
+    }
+
+
+}
